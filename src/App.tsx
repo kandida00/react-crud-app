b1b1259a4f5a4824d4d9fb7ae8eddb82c28b2c7f
Dida <kandida.korintian@salt.co.id>
2024-09-13 00:09:10 +0700

    import excel

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import '@ag-grid-community/styles/ag-theme-quartz.css';
 import { addUser, deleteUser, getUsers, updateUser } from './services/UserService';
 import UserModal from './components/UserModal';
 import moment from 'moment';
+import * as XLSX from 'xlsx';
 
 export interface User {
   id: number;
@@ -80,6 +81,27 @@ function App() {
     gridRef.current!.api.exportDataAsCsv();
   }, []);
 
+  const onBtImport = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files[0]) {
+      const file = e.target.files[0];
+      const reader = new FileReader();
+      reader.onload = (event) => {
+        const data = new Uint8Array(event.target!.result as ArrayBuffer);
+        const workbook = XLSX.read(data, { type: 'array' });
+        const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+        const jsonData = XLSX.utils.sheet_to_json<User>(worksheet);
+
+        const formattedData = jsonData.map((item: any) => ({
+          ...item,
+          birthday: moment(item.birthday).toDate()
+        }));
+
+        setUsers(formattedData);
+      };
+      reader.readAsArrayBuffer(file);
+    }
+  }
+
   useEffect(() => {
     fetchUsers();
   }, [])
@@ -95,6 +117,15 @@ function App() {
         >
           Export to Excel
         </button>
+        <div style={{ display: 'flex', gap: 10 }}>
+          <label>Import from Excel</label>
+          <input
+            type="file"
+            accept=".xlsx,.xls"
+            style={{ marginBottom: "5px" }}
+            onChange={onBtImport}
+          />
+        </div>
       </div>
       <div
         className="ag-theme-quartz"
